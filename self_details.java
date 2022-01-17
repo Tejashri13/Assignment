2fabcfe18a74dd91ceb8118ca5fe511d399cf480
Tejashri <88188090+Tejashri13@users.noreply.github.com>
2022-01-17 20:04:36 +0530

    Add files via upload

---
+++ self_details.java
@@ -0,0 +1,22 @@
+public class self_details {
+
+	public static void main(String[] args) {
+       System.out.println(" My name is Tejashri  Suryawanshi \n"+ " Street Area : DM nagar \n" + " mob no:7066127785 \n "
+       		+ " Passing year : 2021 \n"
+       		+ " Hobbies : Dancing, Playing badminton ");
+              
+       
+	}
+
+}
+
+
+/*
+OUTPUT: 
+---------------
+ My name is Tejashri  Suryawanshi 
+ Street Area : DM nagar 
+ mob no:7066127785 
+ Passing year : 2021 
+ Hobbies : Dancing, Playing badminton 
+*/
